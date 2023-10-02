7894c7d3a5e2737725fc07378ba7f2e1c8437e4a
akash.nair <akash.nair@understand.ai>
2023-10-02 12:59:32 +0200

    Bake tensors into Docker image

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,14 @@
-FROM python:3.11-slim
+FROM python:3.11-slim as base
 
 # Install system dependencies
 RUN apt-get update && apt-get install -y \
     curl
 
 COPY app/ /app
+
+COPY model/ /model
+
+RUN curl -L -o /model/model.safetensors https://huggingface.co/facebook/bart-large-mnli/resolve/main/model.safetensors
 
 RUN pip install --no-cache-dir -r app/requirements.txt
 
--- app/model.py
+++ app/model.py
@@ -1,8 +1,21 @@
-from transformers import pipeline
 from flask import Flask, request, jsonify
+from safetensors import safe_open
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipeline
 
 app = Flask(__name__)
 
-classifier = pipeline(
-    task="zero-shot-classification", model="facebook/bart-large-mnli"
-)
+# Load tensors from the model.safetensors file
+tensors = {}
+with safe_open("../model/model.safetensors", framework="pt", device="cpu") as f:
+    for k in f.keys():
+        tensors[k] = f.get_tensor(k)
+
+# Assume tensors contains the necessary weights for your model.
+# Load your model (replace 'your-model-name' with the actual name or path of your model)
+local_bart_large_mnli_model = AutoModelForSequenceClassification.from_pretrained('../model')
+
+# Load the tokenizer
+tokenizer = AutoTokenizer.from_pretrained('facebook/bart-large-mnli')
+
+# Use the model and tokenizer with Hugging Face's classifier function.
+classifier = pipeline('zero-shot-classification', model=local_bart_large_mnli_model, tokenizer=tokenizer)
