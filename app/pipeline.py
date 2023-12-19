2ca91c85f9f00f2d8f5ad2c0baab64c4ff817611
slashr <2951124+slashr@users.noreply.github.com>
2023-12-19 18:15:35 +0100

    Fix clearml task

--- app/pipeline.py
+++ app/pipeline.py
@@ -12,14 +12,7 @@ import subprocess
 #        docker="dawker/bart-large-mnli:latest",
 #    )
 #
-#    pipe.set_default_execution_queue("jobs_cpu_14gb")
-#
-#
-##    docker_setup_script = """
-##    #!/bin/bash
-##    echo "Running docker setup bash script..."
-##    python api.py
-##    """
+#    pipe.set_default_execution_queue("default")
 #
 #    # add pipeline components
 #    pipe.add_function_step(
@@ -29,32 +22,15 @@ import subprocess
 #        #function_return=["dataset_spec"],
 #        cache_executed_step=False,
 #        docker="dawker/bart-large-mnli:latest",
-#        docker_bash_setup_script=docker_setup_script,
 #        repo="https://github.com/slashr/LLM-Experiment",
 #        repo_branch="clearml",
 #    )
 #
 #    #pipe.start_locally(run_pipeline_steps_locally=True)
-#    pipe.start(queue="jobs_cpu_14gb")
+#    pipe.start(queue="default")
 #
 #    print("pipeline completed")
 #
-
-def execute_in_docker():
-    # This code will run in the Docker container
-    print("Current Working Directory:", os.getcwd())
-    os.chdir("/")
-    print("Changed Working Directory:", os.getcwd())
-    print("Files:", os.listdir(os.getcwd()))
-
-    uname_result = subprocess.run(['uname', '-a'], stdout=subprocess.PIPE, text=True)
-    print("UNAME", uname_result.stdout)
-
-    whoami_result = subprocess.run(['whoami'], stdout=subprocess.PIPE, text=True)
-    print("WHOAMI", whoami_result.stdout)
-
-    df_result = subprocess.run(['df', '-h'], stdout=subprocess.PIPE, text=True)
-    print("DF", df_result.stdout)
 
 def execute_remotely(project_name="akash_project", task_name="akash_task"):
     # TODO - add type, tags
@@ -62,25 +38,13 @@ def execute_remotely(project_name="akash_project", task_name="akash_task"):
     task.set_base_docker(
         docker_image="dawker/bart-large-mnli:latest"
     )
-    # task.execute_remotely(queue_name="k8s_scheduler")
     task.execute_remotely(queue_name="default")
-    execute_in_docker()
-
-
+    # Change the directory to where the app is stored
+    # Default directory is /
+    os.chdir("/app")
+    from api import classify
+    classify()
 
 if __name__ == "__main__":
     # Comment out to run locally
     execute_remotely(project_name="akash_project", task_name="akash_test")
-#    current_directory = os.getcwd()
-#    print("Current Working Directory:", current_directory)
-#    os.chdir("/") 
-#    current_directory = os.getcwd()
-#    print("Changed Working Directory:", current_directory)
-#    print("Files:", os.listdir(current_directory))
-#    uname_result = subprocess.run(['uname', '-a'], stdout=subprocess.PIPE, text=True)
-#    print("UNAME", uname_result) 
-#    whoami_result = subprocess.run(['whoami'], stdout=subprocess.PIPE, text=True)
-#    print("WHOAMI", whoami_result)
-#    df_result = subprocess.run(['df', '-h'], stdout=subprocess.PIPE, text=True)
-#    print("DF", df_result)
-#
